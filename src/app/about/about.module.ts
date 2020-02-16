9597b9d3c7ed9607f8d3b4821fb5362470ce18ab
LucasRomero <patolucasromero@gmail.com>
2020-02-16 19:54:19 -0300

    LandingPage

---
+++ src/app/about/about.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AboutRoutingModule } from './about-routing.module';
+import { AboutComponent } from './about/about.component';
+import { LinksComponent } from './about/links/links.component';
+import { InfoComponent } from './about/info/info.component';
+
+
+@NgModule({
+  declarations: [AboutComponent, LinksComponent, InfoComponent],
+  imports: [
+    CommonModule,
+    AboutRoutingModule,
+    AboutComponent,
+    LinksComponent,
+    InfoComponent
+  ]
+})
+export class AboutModule { }
